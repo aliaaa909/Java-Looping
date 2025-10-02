6f19b8efca2f7fdf0760cf3c1b8d6af3df4fe879
liaaaa ୨୧ <2410631170006@student.unsika.ac.id>
2025-10-02 19:40:08 +0700

    Update and rename BMI.java to BMIAPP.java

--- BMIAPP.java
+++ BMIAPP.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class BmiApp {
+public class BMIAPP {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String repeat; 
